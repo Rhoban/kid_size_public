664b41e297921399863dff05bd9c7d081cbce914
Patxi Laborde-Zubieta <patxi.laborde.zubieta@gmail.com>
2018-05-07 13:56:55 +0200

    Update manufacturing pieces

--- Manufacturing/calibration_set/print_aruco.py
+++ Manufacturing/calibration_set/print_aruco.py
@@ -2,22 +2,46 @@
 
 import sys
 import subprocess
+import glob
 
 args=sys.argv
 if len(args)<2:
     print(
 """
 Two arguments should be given:
-    - a size : big or small.
-    - a number or a list of numbers
-        * a number between 0 and 249 (included) if big
-        * a list of numbers (max=6) between 0 and 249 (included)
+    - a size : big, small or multiple_small.
+    - if big, give a number between 0 and 249 (included)
+    - if small, give a list of numbers (max=6) between 0 and 249 (included)
+    - if multiple_small give the start and the end of the interval (included)
 """)
     sys.exit(1)
 else:
     def bash_command(cmd):
         p=subprocess.Popen(cmd, shell=True, executable='/bin/bash')
         p.wait()
+
+    if args[1]=="multiple_small":
+      start=int(args[2])
+      end=int(args[3])
+      l=[str(i) for i in range(start,end+1)]
+      s=""
+      for i in range(len(l)):
+        s+=" "+l[i]
+        if i%6==5 or i==len(l)-1:
+          bash_command("./print_aruco.py small%s"%s)
+          s=""
+      out=glob.glob("aruco_small_*")
+      cmd="pdftk "
+      for file in out:
+        cmd+=file+" "
+      cmd+="cat output aruco_multiple_small_%d_%d.pdf"%(start,end)
+      bash_command(cmd)
+
+      cmd="rm aruco_small_*"
+      bash_command(cmd)
+
+
+      sys.exit(0)
 
     latex_file="""\documentclass[a4paper]{article}
     \pagenumbering{gobble}
@@ -38,7 +62,7 @@ else:
             \\begin{textblock}{3}(%f,%f)
                 \includegraphics[width=%smm]{tmp/aruco_mip_36h12_%s.png}
             \end{textblock}"""%(float((210-size)/2),float((297-size)/2),size,n.zfill(5))
-    else:
+    elif args[1]=="small":
         size=90
         border=9
         total=size+border
@@ -46,7 +70,7 @@ else:
         for i in range(len(args[2:])):
             latex_file+="""\\begin{textblock}{0}(%d,%d)
                 \setlength{\\fboxsep}{%dmm}%%
-                \setlength{\\fboxrule}{.1mm}%%
+                \setlength{\\fboxrule}{0mm}%%
                 \\fbox{%%
                     \includegraphics[width=%smm]{tmp/aruco_mip_36h12_%s.png}%%
                 }%%
