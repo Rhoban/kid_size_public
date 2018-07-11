0797045ce13f91ab63490290cf71189a3ed31a0e
medrimonia <medrimonia@gmail.com>
2018-07-11 14:06:49 +0200

    Removing deprecated tests

--- Vision/Test/LocalisationTest.cpp
+++
@@ -1,6 +0,0 @@
-#include "tests/runTests.h"
-#include "TestCameraState.hpp"
-
-using namespace Vision::Utils;
-
-int main() { runTests<TestCameraState>("Testing CameraState"); }
--- Vision/Test/Sources.cmake
+++
@@ -1,3 +0,0 @@
-set (SOURCES
-  TestCameraState.cpp
-)
