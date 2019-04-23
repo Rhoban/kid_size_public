de26c01cfe64c99a4a2e97027a3edd3b5dd58de4
medrimonia <medrimonia@gmail.com>
2019-04-23 16:45:32 +0200

    Fix of FieldObservations limits

--- Vision/Localisation/Field/FieldObservation.cpp
+++ Vision/Localisation/Field/FieldObservation.cpp
@@ -24,7 +24,7 @@ double FieldObservation::potential(const FieldPosition& p) const
   // TODO: this should not be done, approaches should take it into account
   double dx = abs(pos.x) - Constants::field.field_length / 2;
   // Accepting to go slightly out of the field laterally
-  double dy = abs(pos.y) - (Constants::field.field_width / 2 - Constants::field.border_strip_width_y);
+  double dy = abs(pos.y) - (Constants::field.field_width / 2 + Constants::field.border_strip_width_y);
   if (dx > 0 || dy > 0)
   {
     return pError;
