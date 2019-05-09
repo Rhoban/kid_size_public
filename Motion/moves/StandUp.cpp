f073ff770041496aedad9a2d20188a4c5602875c
Gregwar <g.passault@gmail.com>
2019-05-09 14:52:48 +0200

    Lay down speed

--- Motion/moves/StandUp.cpp
+++ Motion/moves/StandUp.cpp
@@ -102,6 +102,7 @@ void StandUp::step(float elapsed)
         if (layDown)
         {
           splines = Function::fromFile("lay_down.json");
+          speed = 1.0;
         }
         else
         {
