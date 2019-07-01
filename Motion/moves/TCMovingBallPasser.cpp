37a0eb6766347609a065b432dd8abbd07c719bcf
Gregwar <g.passault@gmail.com>
2019-07-01 22:39:11 +1000

    Reactive kicker is reacting on mate kick instead of always rising leg

--- Motion/moves/TCMovingBallPasser.cpp
+++ Motion/moves/TCMovingBallPasser.cpp
@@ -32,6 +32,7 @@ void TCMovingBallPasser::onStart()
   head->setDisabled(false);
 
   // XXX: This is hacky
+  RhIO::Root.setFloat("/moves/kick/warmup", 2);
   RhIO::Root.setFloat("/moves/walk/maxStep", 0.06);
   RhIO::Root.setFloat("/moves/walk/maxLateral", 0.03);
   RhIO::Root.setFloat("/moves/walk/maxRotation", 15);
